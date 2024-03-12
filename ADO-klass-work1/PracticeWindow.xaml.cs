a7576da13bf82ee186e1d8a1abb1cf5a49e1d740
Artem <artemzhdanov1299@gmail.com>
2024-03-12 11:35:47 +0200

    final

--- ADO-klass-work1/PracticeWindow.xaml.cs
+++ ADO-klass-work1/PracticeWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ADO_klass_work1.EfContext;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,6 +27,37 @@ namespace ADO_klass_work1
         }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            DateTime data = DateTime.Today.AddYears(-1).Date;
+
+            ///////1
+            Questable_label.Content = "";
+            DateTime MinSale = App.EfDataContext.Sales
+                .Where(S => S.SaleDt.Date == data)
+                .Min(S => S.SaleDt);
+            Questable_label.Content = MinSale.TimeOfDay.ToString();
+
+            ////////2
+            Questable_label2.Content = "";
+            DateTime MaxSale = App.EfDataContext.Sales
+                .Where(S => S.SaleDt.Date == data)
+                .Max(S => S.SaleDt);
+            Questable_label2.Content = MaxSale.TimeOfDay.ToString();
+
+            ////////3 (max)
+            int MaxProd = App.EfDataContext.Sales
+            .Where(S => S.SaleDt.Date == data)
+            .Max(S => S.Quantity);
+            Questable_label3.Content = MaxProd;
+
+            ///////4(среденее midle)
+            int MidleProduct = (int)App.EfDataContext.Sales
+            .Where(s => s.SaleDt.Year == data.Year && s.SaleDt.Month == data.Month && s.SaleDt.Day == data.Day)
+            .Select(s => s.Quantity)
+            .Average();
+            Questable_label4.Content = MidleProduct;
+
+            ///////5(среденее midle)
+            
         }
     }
 }
